1374ccc90dd8e8ce4bf6492ec16c2e181645789e
Igor Durbek <igor@igordurbek.com>
2023-06-24 22:44:02 +0200

    Backend: RController

---
+++ HamsterChessBackend/src/main/java/practice/igoroffline/hamsterChessBackend/HamsterChessBackendApplication.java
@@ -0,0 +1,13 @@
+package practice.igoroffline.hamsterChessBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsterChessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamsterChessBackendApplication.class, args);
+	}
+
+}
---
+++ HamsterChessBackend/src/test/java/practice/igoroffline/hamsterChessBackend/HamsterChessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.igoroffline.hamsterChessBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsterChessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
